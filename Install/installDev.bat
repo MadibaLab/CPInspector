4fd6637591a736dfc4804c855ff595e6d9e80917
sali123 <39350108+sali123@users.noreply.github.com>
2018-09-09 09:33:37 -0400

    remove chrome-remote installation

--- Install/installDev.bat
+++ Install/installDev.bat
@@ -2,5 +2,3 @@ python -m pip install --upgrade pip
 pip install selenium==3.14.0
 pip install six
 pip install pywin32
-cd C:\Hotspot\extensions\chrome_extensions\DFPM
-npm install chrome-remote-interface -g
