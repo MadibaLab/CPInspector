731985201beb2514c2a248d1c422acb549245cad
sali123 <39350108+sali123@users.noreply.github.com>
2018-09-10 00:21:28 -0400

    update installation

--- Install/installDev.bat
+++ Install/installDev.bat
@@ -3,4 +3,4 @@ pip install selenium==3.14.0
 pip install six
 pip install pywin32
 cd C:\Hotspot\extensions\chrome_extensions\DFPM
-npm install chrome-remote-interface -g
+npm install chrome-remote-interface 
